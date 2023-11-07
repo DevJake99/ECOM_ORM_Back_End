10dd46aff096537d9f4da3f270c82e638a611638
DevJake99 <jacobg4@uci.edu>
2023-11-07 15:09:35 -0800

    database is working and synced to server

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,35 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+-- DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+-- CREATE DATABASE ecommerce_db;
 
-CREATE TABLE Product (
-  id INT AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(30) NOT NULL,
-  price DECIMAL(6, 2) NOT NULL,
-  stock INT NOT NULL, DEFAULT 10,
-  category_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (category_id) REFERENCES Category(id)
-)
 
 CREATE TABLE Category (
-    id INT AUTO_INCREMENT NOT NULL,
-    category_name VARCHAR(30) NOT NULL,
-
-)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    category_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE Tag (
-    id INT AUTO_INCREMENT NOT NULL, 
-    tag_name: VARCHAR(30) NOT NULL,
-)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    tag_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Product (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  product_name VARCHAR(30) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT DEFAULT 10 NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+
 
 CREATE TABLE Product_Tag (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     product_id INT, 
     tag_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES Product(id),
     FOREIGN KEY (tag_id) REFERENCES Tag(id)
 );
